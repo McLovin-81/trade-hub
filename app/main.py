19475b05702135538cf375a7068c11b903ca689c
Cristian Felipe <estilo-callejero@hotmail.de>
2024-09-09 16:49:39 +0200

    How to run in main.py

--- app/main.py
+++ app/main.py
@@ -4,3 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True, ssl_context=('instance/certs/cert.pem', 'instance/certs/key.pem'))
+
+# run: python3 -m app.main
