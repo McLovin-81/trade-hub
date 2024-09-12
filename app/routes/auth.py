678718020ade73ca5d9f18657ea770bbce321d71
Dacowa <dannydawnbringer@gmail.com>
2024-09-12 08:08:25 +0200

    testing new transaction query

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -66,7 +66,7 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     
-    if request.method == 'GET':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
